ba5b5b88737b329991c449b8dca74c56a7a75723
[Yildizhan YAZAR] <[yildizhann.yazarr@gmail.com]>
2022-09-04 15:04:27 +0300

    end reactjs weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Form from "./component/form";
+import { Info } from "./component/info";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [info, setInfo] = useState([]);
+   const [state, setState] = useState(false);
+   return (
+      <div className='App'>
+         <Form setInfo={setInfo} setState={setState} />
+         <Info info={info} state={state} />
+      </div>
+   );
 }
-
 export default App;
---
+++ src/component/form.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function Form({ setInfo, setState }) {
+   const [city, setCity] = useState("");
+   const handleChange = async () => {
+      const API = "Buraya API key ekle";
+      const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=metric&lang=tr`;
+      await axios(baseURL).then((res) => setInfo(res.data));
+      setState(true);
+   };
+   return (
+      <div>
+         <h1>Hava Durumu</h1>
+         <form
+            onSubmit={(e) => {
+               e.preventDefault();
+               handleChange();
+            }}
+         >
+            <div className='form'>
+               <input
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  className='inputText'
+                  type='text'
+                  placeholder='Hava durumunu öğrenmek istediğiniz şehiri buraya yazın'
+               />
+            </div>
+            <div className='btnDiv'>
+               <button type='submit' className='btn'>
+                  Verileri Getir
+               </button>
+            </div>
+         </form>
+      </div>
+   );
+}
